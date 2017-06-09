11ba7b1a979ced3db5ef8393fb9bb75289d21e25
Andrew Adams <abadams@calculon.mtv.corp.google.com>
2017-06-09 11:02:31 -0700

    Simplify coroutine test

--- test/generator/async_coroutine_aottest.cpp
+++ test/generator/async_coroutine_aottest.cpp
@@ -15,18 +15,12 @@ struct execution_context {
     char *stack_bottom = NULL;
     char *stack = NULL;
     int priority = 0;
-    enum {done, running, suspended, waiting_on_semaphore} state;
 };
 
 int context_switches = 0;
 
 void switch_context(execution_context *from, execution_context *to) {
     context_switches++;
-    if (to->state != execution_context::suspended) {
-        printf("Trying to switch to context in state %d\n", (int)(to->state));
-        abort();
-    }
-    to->state = execution_context::running;
 
     // To switch contexts, we'll push a return address onto our own
     // stack, switch to the target stack, and then issue a ret
@@ -97,8 +91,6 @@ void call_in_new_context(execution_context *from,
     memset(to->stack_bottom, 0, sz);
     to->stack = to->stack_bottom + sz;
     to->stack = (char *)(((uintptr_t)to->stack) & ~((uintptr_t)(15)));
-    to->state = execution_context::running;
-    from->state = execution_context::suspended;
 
     // printf("Calling %p(%p, %p, %p) in a new context at %p\n", f, from, to, arg, to->stack);
 
@@ -167,7 +159,7 @@ struct compare_contexts {
 
 std::priority_queue<execution_context *,
                     std::vector<execution_context *>,
-                    compare_contexts> contexts;
+                    compare_contexts> runnable_contexts;
 
 std::vector<execution_context *> dead_contexts;
 
@@ -176,21 +168,9 @@ execution_context scheduler_context;
 void scheduler(execution_context *parent, execution_context *this_context, void *arg) {
     // The first time this is called is just to set up the scheduler's
     // context so immediately transfer control back to the parent.
-    this_context->state = execution_context::suspended;
     switch_context(this_context, parent);
 
     while (1) {
-        // Dump the queue for debugging
-        decltype(contexts) other;
-        printf("Priority queue:\n");
-        while (!contexts.empty()) {
-            auto c = contexts.top();
-            contexts.pop();
-            printf("  %d : %p (%d)\n", c->priority, c, (int)c->state);
-            other.push(c);
-        }
-        other.swap(contexts);
-
         // Clean up any finished contexts
         for (execution_context *ctx: dead_contexts) {
             printf("Destroying context %p\n", ctx);
@@ -203,100 +183,14 @@ void scheduler(execution_context *parent, execution_context *this_context, void 
         dead_contexts.clear();
 
         // Run the next highest-priority context
-        execution_context *next = contexts.top();
-        contexts.pop();
-        if (next->state != execution_context::suspended) {
-            printf("execution context shouldn't be in queue: %p\n", next);
-            abort();
-        }
-
+        execution_context *next = runnable_contexts.top();
+        runnable_contexts.pop();
         printf("Running context %p with priority %d\n", next, next->priority);
-        this_context->state = execution_context::suspended;
         switch_context(this_context, next);
     }
 }
 
-struct do_one_task_arg {
-    halide_parallel_task_t *task;
-    halide_semaphore_t *parent_semaphore;
-};
-
-void do_one_task(execution_context *parent, execution_context *this_context, void *arg) {
-    do_one_task_arg *task_arg = (do_one_task_arg *)arg;
-    halide_parallel_task_t *task = task_arg->task;
-    halide_semaphore_t *parent_sema = task_arg->parent_semaphore;
-    this_context->priority = -(task->min_threads);
-
-    for (int i = task->min; i < task->min + task->extent; i++) {
-        // Try to acquire the semaphores
-        for (int j = 0; j < task->num_semaphores; j++) {
-            my_semaphore *sema = (my_semaphore *)(task->semaphores[j].semaphore);
-            while (!halide_semaphore_try_acquire(task->semaphores[j].semaphore, task->semaphores[j].count)) {
-                if (!sema->waiter) {
-                    printf("Sleeping context %p on a semaphore\n", this_context);
-                    sema->waiter = this_context;
-                    this_context->state = execution_context::waiting_on_semaphore;
-                } else {
-                    printf("Someone else (%p) already sleeping on my semaphore %p\n", sema->waiter, this_context);
-                    abort();
-                    this_context->priority--;
-                    this_context->state = execution_context::suspended;
-                    contexts.push(this_context);
-                }
-                switch_context(this_context, &scheduler_context);
-                if (sema->waiter == this_context) {
-                    printf("Woke up %p still attached to semaphore!\n", this_context);
-                    abort();
-                } else {
-                    printf("Woke up %p not attached to semaphore\n", this_context);
-                }
-            }
-            printf("Successfully acquired semaphore %p\n", this_context);
-        }
-        printf("Entering Halide\n");
-        task->fn(nullptr, i, task->closure);
-        printf("Leaving Halide\n");
-    }
-    task->extent = 0;
-    this_context->state = execution_context::done;
-    halide_semaphore_release(parent_sema, 1);
-    dead_contexts.push_back(this_context);
-    switch_context(this_context, &scheduler_context);
-    // Shouldn't get scheduled again
-    printf("Scheduled a finished context\n");
-    abort();
-}
-
-int do_par_tasks(void *user_context, int num_tasks, halide_parallel_task_t *tasks) {
-
-    // Make this stack frame schedulable
-    execution_context *this_context = new execution_context;
-    for (int i = 0; i < num_tasks; i++) {
-        this_context->priority -= tasks[i].min_threads;
-    }
-    this_context->state = execution_context::running;
-
-    halide_semaphore_t parent_sema;
-    halide_semaphore_init(&parent_sema, 1 - num_tasks);
-
-    // Queue up the children
-    for (int i = 0; i < num_tasks; i++) {
-        execution_context *ctx = new execution_context;
-        do_one_task_arg arg = {tasks + i, &parent_sema};
-        this_context->state = execution_context::suspended;
-        contexts.push(this_context);
-        call_in_new_context(this_context, ctx, do_one_task, &arg);
-    }
-
-    if (!halide_semaphore_try_acquire(&parent_sema, 1)) {
-        ((my_semaphore *)(&parent_sema))->waiter = this_context;
-        this_context->state = execution_context::waiting_on_semaphore;
-        switch_context(this_context, &scheduler_context);
-    }
-    return 0;
-}
-
-
+// Implementations of the required Halide semaphore calls
 int semaphore_init(halide_semaphore_t *s, int count) {
     my_semaphore *sema = (my_semaphore *)s;
     sema->count = count;
@@ -317,18 +211,79 @@ bool semaphore_try_acquire(halide_semaphore_t *s, int count) {
 int semaphore_release(halide_semaphore_t *s, int count) {
     my_semaphore *sema = (my_semaphore *)s;
     sema->count += count;
-    if (sema->waiter) {
+    if (sema->waiter && sema->count > 0) {
         // Re-enqueue the blocked context
         printf("Waking context %p\n", sema->waiter);
-        if (sema->waiter->state != execution_context::waiting_on_semaphore) {
-            printf("Waiting semaphore in wrong state: %d\n", (int)(sema->waiter->state));
-            abort();
-        }
-        sema->waiter->state = execution_context::suspended;
-        contexts.push(sema->waiter);
+        runnable_contexts.push(sema->waiter);
         sema->waiter = nullptr;
     }
     return sema->count;
+}
+
+// A blocking version of semaphore acquire that enters the task system
+void semaphore_acquire(execution_context *this_context, halide_semaphore_t *s, int count) {
+    my_semaphore *sema = (my_semaphore *)s;
+    while (sema->count < count) {
+        if (sema->waiter) {
+            // We don't generate IR with competing acquires
+            printf("Semaphore contention!\n");
+            abort();
+        }
+        sema->waiter = this_context;
+        switch_context(this_context, &scheduler_context);
+    }
+    sema->count -= count;
+}
+
+
+
+struct do_one_task_arg {
+    halide_parallel_task_t *task;
+    halide_semaphore_t *parent_semaphore;
+};
+
+void do_one_task(execution_context *parent, execution_context *this_context, void *arg) {
+    do_one_task_arg *task_arg = (do_one_task_arg *)arg;
+    halide_parallel_task_t *task = task_arg->task;
+    halide_semaphore_t *parent_sema = task_arg->parent_semaphore;
+    this_context->priority = -(task->min_threads);
+
+    // This is a single-threaded runtime, so treat all loops as serial.
+    for (int i = task->min; i < task->min + task->extent; i++) {
+        // Try to acquire the semaphores
+        for (int j = 0; j < task->num_semaphores; j++) {
+            semaphore_acquire(this_context, task->semaphores[j].semaphore, task->semaphores[j].count);
+        }
+        task->fn(nullptr, i, task->closure);
+    }
+    halide_semaphore_release(parent_sema, 1);
+    dead_contexts.push_back(this_context);
+    switch_context(this_context, &scheduler_context);
+    printf("Scheduled dead context!\n");
+    abort();
+}
+
+int do_par_tasks(void *user_context, int num_tasks, halide_parallel_task_t *tasks) {
+    // Make this context schedulable.
+    execution_context *this_context = new execution_context;
+    for (int i = 0; i < num_tasks; i++) {
+        this_context->priority -= tasks[i].min_threads;
+    }
+
+    // Make a semaphore to wake this context when the children are done.
+    halide_semaphore_t parent_sema;
+    halide_semaphore_init(&parent_sema, 1 - num_tasks);
+
+    // Queue up the children. Run each up until the first stall.
+    for (int i = 0; i < num_tasks; i++) {
+        execution_context *ctx = new execution_context;
+        do_one_task_arg arg = {tasks + i, &parent_sema};
+        runnable_contexts.push(this_context);
+        call_in_new_context(this_context, ctx, do_one_task, &arg);
+    }
+
+    semaphore_acquire(this_context, &parent_sema, 1);
+    return 0;
 }
 
 int main(int argc, char **argv) {
@@ -363,7 +318,16 @@ int main(int argc, char **argv) {
 
     printf("Context switches: %d\n", context_switches);
     printf("Max stacks allocated: %d\n", stacks_high_water);
-    printf("Stacks still allocated: %d\n", stacks_allocated);
+    printf("Stacks still allocated: %d (1 expected)\n", stacks_allocated);
+    free(scheduler_context.stack_bottom);
+    if (stacks_high_water > 50) {
+        printf("Runaway stack allocation!\n");
+        return -1;
+    }
+    if (stacks_allocated != 1) {
+        printf("Zombie stacks\n");
+        return -1;
+    }
 
     printf("Success!\n");
     return 0;
