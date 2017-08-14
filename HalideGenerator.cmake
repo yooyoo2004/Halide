5965e9e225756a20e2bd351470361ca19051384f
Steven Johnson <srj@google.com>
2017-08-14 10:42:01 -0700

    Still more

--- HalideGenerator.cmake
+++ HalideGenerator.cmake
@@ -1,5 +1,13 @@
 include(CMakeParseArguments)
 
+if("${HALIDE_SRC_DIR}" STREQUAL "")
+  set(HALIDE_SRC_DIR "${CMAKE_SOURCE_DIR}")
+endif()
+
+define_property(TARGET PROPERTY HALIDE_GENERATOR_NAME
+                BRIEF_DOCS "Internal use by Halide build rules"
+                FULL_DOCS "Internal use by Halide build rules")
+
 function(_halide_generator_genfiles_dir NAME OUTVAR)
   set(GENFILES_DIR "${CMAKE_BINARY_DIR}/genfiles/${NAME}")
   file(MAKE_DIRECTORY "${GENFILES_DIR}")
@@ -8,20 +16,20 @@ endfunction()
 
 function(_halide_generator_add_exec_generator_target EXEC_TARGET)
   set(options )
-  set(oneValueArgs GENERATOR GENFILES_DIR)
+  set(oneValueArgs GENERATOR_BINARY GENFILES_DIR)
   set(multiValueArgs OUTPUTS GENERATOR_ARGS)
   cmake_parse_arguments(args "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   if(MSVC)
     # In MSVC, the generator executable will be placed in a configuration specific
     # directory specified by ${CMAKE_CFG_INTDIR}.
-    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/${args_GENERATOR}${CMAKE_EXECUTABLE_SUFFIX}")
+    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/${args_GENERATOR_BINARY}${CMAKE_EXECUTABLE_SUFFIX}")
   elseif(XCODE)
     # In Xcode, the generator executable will be placed in a configuration specific
     # directory, so the Xcode variable $(CONFIGURATION) is passed in the custom build script.
-    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/$(CONFIGURATION)/${args_GENERATOR}${CMAKE_EXECUTABLE_SUFFIX}")
+    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/$(CONFIGURATION)/${args_GENERATOR_BINARY}${CMAKE_EXECUTABLE_SUFFIX}")
   else()
-    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/${args_GENERATOR}${CMAKE_EXECUTABLE_SUFFIX}")
+    set(EXEC_PATH "${CMAKE_BINARY_DIR}/bin/${args_GENERATOR_BINARY}${CMAKE_EXECUTABLE_SUFFIX}")
   endif()
 
   # This "target" will always be built.
@@ -29,10 +37,10 @@ function(_halide_generator_add_exec_generator_target EXEC_TARGET)
   set_target_properties(${EXEC_TARGET} PROPERTIES FOLDER "generator")
   add_custom_command(
     OUTPUT ${args_OUTPUTS}
-    DEPENDS ${args_GENERATOR}
+    DEPENDS ${args_GENERATOR_BINARY}
     COMMAND ${EXEC_PATH} ${args_GENERATOR_ARGS}
     WORKING_DIRECTORY ${args_GENFILES_DIR}
-    COMMENT "Executing Generator ${args_GENERATOR} with args ${args_GENERATOR_ARGS}..."
+    # COMMENT "Executing Generator ${args_GENERATOR_BINARY} with args ${args_GENERATOR_ARGS}..."
   )
   foreach(OUT ${args_OUTPUTS})
     set_source_files_properties(${OUT} PROPERTIES GENERATED TRUE)
@@ -40,7 +48,6 @@ function(_halide_generator_add_exec_generator_target EXEC_TARGET)
 endfunction()
 
 function(halide_generator NAME)
-# TODO GENERATOR_NAME
 # TODO DEPS other than stubs
   set(oneValueArgs LIBHALIDE GENERATOR_NAME)
   set(multiValueArgs SRCS DEPS INCLUDES)
@@ -56,6 +63,10 @@ function(halide_generator NAME)
 
   # BASENAME = strip_suffix(${NAME}, ".generator")
   string(REGEX REPLACE "\\.generator*$" "" BASENAME ${NAME})
+
+  if ("${args_GENERATOR_NAME}" STREQUAL "")
+    set(args_GENERATOR_NAME "${BASENAME}")
+  endif()
 
   # Use Object Libraries to so that Generator registration isn't dead-stripped away
   set(OBJLIB "${NAME}_library")
@@ -79,9 +90,9 @@ function(halide_generator NAME)
     target_include_directories("${OBJLIB}" PRIVATE $<TARGET_PROPERTY:${DEP},INTERFACE_INCLUDE_DIRECTORIES>)
   endforeach()
 
-  add_executable("${NAME}_binary" "${CMAKE_SOURCE_DIR}/tools/GenGen.cpp" ${ALLDEPS})
+  add_executable("${NAME}_binary" "${HALIDE_SRC_DIR}/tools/GenGen.cpp" ${ALLDEPS})
   target_link_libraries("${NAME}_binary" PRIVATE ${args_LIBHALIDE} z ${CMAKE_DL_LIBS} ${CMAKE_THREAD_LIBS_INIT})
-  target_include_directories("${NAME}_binary" PRIVATE "${CMAKE_SOURCE_DIR}/include" "${CMAKE_SOURCE_DIR}/tools")
+  target_include_directories("${NAME}_binary" PRIVATE "${HALIDE_SRC_DIR}/include" "${HALIDE_SRC_DIR}/tools")
   set_target_properties("${NAME}_binary" PROPERTIES FOLDER "generator")
   if (MSVC)
     set_target_properties(${NAME} PROPERTIES LINK_FLAGS "/ignore:4006 /ignore:4088")
@@ -95,26 +106,36 @@ function(halide_generator NAME)
 
   _halide_generator_add_exec_generator_target(
     "${NAME}_stub_gen"
-    GENERATOR "${NAME}_binary"
+    GENERATOR_BINARY "${NAME}_binary"
     GENERATOR_ARGS   "${GENERATOR_EXEC_ARGS}"
     GENFILES_DIR     "${GENFILES_DIR}"
     OUTPUTS          "${STUB_HDR}"
   )
+  set_property(TARGET "${NAME}_stub_gen" PROPERTY HALIDE_GENERATOR_NAME "${args_GENERATOR_NAME}")
 
   # Make a header-only library that exports the include path
   add_library("${NAME}" INTERFACE)
   add_dependencies("${NAME}" "${NAME}_stub_gen")
   set_target_properties("${NAME}" PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${GENFILES_DIR})
+  # TODO -- not allowed, but this is what we need, yuck
+  # set_target_properties("${NAME}" PROPERTIES INTERFACE_LINK_LIBRARIES ${OBJLIB})
 endfunction(halide_generator)
 
+# TODO do we want to use halide_target_features instead of GENERATOR_HALIDE_TARGET?
 function(halide_library_from_generator BASENAME)
   set(options )
-  set(oneValueArgs GENERATOR GENERATOR_NAME GENERATED_FUNCTION)
+  set(oneValueArgs GENERATOR FUNCTION_NAME GENERATOR_HALIDE_TARGET)
   set(multiValueArgs GENERATOR_ARGS GENERATOR_OUTPUTS FILTER_DEPS)
   cmake_parse_arguments(args "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  if (args_GENERATED_FUNCTION STREQUAL "")
-    set(args_GENERATED_FUNCTION ${args_GENERATOR_NAME})
+  if ("${args_GENERATOR}" STREQUAL "")
+    set(args_GENERATOR "${BASENAME}.generator")
+  endif()
+
+  get_property(GENERATOR_NAME TARGET "${args_GENERATOR}_stub_gen" PROPERTY HALIDE_GENERATOR_NAME)
+
+  if ("${args_FUNCTION_NAME}" STREQUAL "")
+    set(args_FUNCTION_NAME "${BASENAME}")
   endif()
 
   # Create a directory to contain generator specific intermediate files
@@ -126,16 +147,20 @@ function(halide_library_from_generator BASENAME)
   set(FILTER_CPP "${BASENAME}.cpp")
 
   set(GENERATOR_EXEC_ARGS "-o" "${GENFILES_DIR}")
-  if (NOT ${args_GENERATED_FUNCTION} STREQUAL "")
-    list(APPEND GENERATOR_EXEC_ARGS "-f" "${args_GENERATED_FUNCTION}" )
+  if (NOT ${args_FUNCTION_NAME} STREQUAL "")
+    list(APPEND GENERATOR_EXEC_ARGS "-f" "${args_FUNCTION_NAME}" )
   endif()
-  if (NOT ${args_GENERATOR_NAME} STREQUAL "")
-    list(APPEND GENERATOR_EXEC_ARGS "-g" "${args_GENERATOR_NAME}")
+  if (NOT ${GENERATOR_NAME} STREQUAL "")
+    list(APPEND GENERATOR_EXEC_ARGS "-g" "${GENERATOR_NAME}")
   endif()
   if (NOT "${args_GENERATOR_OUTPUTS}" STREQUAL "")
     string(REPLACE ";" "," _tmp "${args_GENERATOR_OUTPUTS}")
     list(APPEND GENERATOR_EXEC_ARGS "-e" ${_tmp})
   endif()
+  if ("${args_GENERATOR_HALIDE_TARGET}" STREQUAL "")
+    set(args_GENERATOR_HALIDE_TARGET "host")
+  endif()
+  list(APPEND GENERATOR_EXEC_ARGS "target=${args_GENERATOR_HALIDE_TARGET}")
   # GENERATOR_ARGS always come last
   list(APPEND GENERATOR_EXEC_ARGS ${args_GENERATOR_ARGS})
 
@@ -162,7 +187,7 @@ function(halide_library_from_generator BASENAME)
 
   _halide_generator_add_exec_generator_target(
     "${BASENAME}_lib_gen"
-    GENERATOR "${args_GENERATOR}_binary"
+    GENERATOR_BINARY "${args_GENERATOR}_binary"
     GENERATOR_ARGS   "${GENERATOR_EXEC_ARGS}"
     GENFILES_DIR     ${GENFILES_DIR}
     OUTPUTS          ${OUTPUTS}
@@ -183,7 +208,7 @@ function(halide_library_from_generator BASENAME)
   # ------ Code to build the RunGen target
 
   set(RUNGEN "${BASENAME}.rungen")
-  add_executable("${RUNGEN}" "${CMAKE_SOURCE_DIR}/tools/RunGenStubs.cpp")
+  add_executable("${RUNGEN}" "${HALIDE_SRC_DIR}/tools/RunGenStubs.cpp")
   target_compile_definitions("${RUNGEN}" PRIVATE "-DHL_RUNGEN_FILTER_HEADER=\"${BASENAME}.h\"")
   target_link_libraries("${RUNGEN}" PRIVATE HalideToolsRunGen "${BASENAME}" ${args_FILTER_DEPS})
 
@@ -197,6 +222,27 @@ function(halide_library_from_generator BASENAME)
   set_target_properties("${BASENAME}.run" PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
 endfunction(halide_library_from_generator)
+
+function(halide_library NAME)
+  set(oneValueArgs LIBHALIDE GENERATOR_NAME FUNCTION_NAME GENERATOR_HALIDE_TARGET)
+  set(multiValueArgs SRCS GENERATOR_DEPS FILTER_DEPS INCLUDES GENERATOR_ARGS GENERATOR_OUTPUTS)
+  cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  halide_generator("${NAME}.generator"
+                   SRCS ${args_SRCS}
+                   DEPS ${args_GENERATOR_DEPS}
+                   INCLUDES ${args_INCLUDES}
+                   LIBHALIDE ${args_LIBHALIDE}
+                   GENERATOR_NAME ${args_GENERATOR_NAME})
+
+  halide_library_from_generator("${NAME}"
+                   DEPS ${args_FILTER_DEPS}
+                   GENERATOR "${NAME}.generator"
+                   FUNCTION_NAME ${args_FUNCTION_NAME}
+                   GENERATOR_HALIDE_TARGET ${args_GENERATOR_HALIDE_TARGET}
+                   GENERATOR_ARGS ${args_GENERATOR_ARGS}
+                   GENERATOR_OUTPUTS ${args_GENERATOR_OUTPUTS})
+endfunction(halide_library)
 
 function(halide_add_aot_cpp_dependency TARGET AOT_LIBRARY_TARGET)
   _halide_generator_genfiles_dir(${AOT_LIBRARY_TARGET} GENFILES_DIR)
